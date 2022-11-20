6ba711d4f22a7832da6866ac3923dfb1b862d212
Vincent Medrano <90278614+VincentMed@users.noreply.github.com>
2022-11-20 15:44:05 -0800

    Update financeSubclass.py

--- financeSubclass.py
+++ financeSubclass.py
@@ -13,16 +13,19 @@ class loan(object):
         
     def setPV(self, PV):
         self._PV = PV
-        print('present value = ', self._PV)
+        print('Amount Borrowed = ', self._PV)
+        return self._PV
         
     def setRate(self, ratePct):
         #set interest, apr
         self._ratePct = ratePct
         print('APR = ', self._ratePct,'%')
+        return self._ratePct
         
     def setMonths(self, months):
         self._months = months
         print(self._months, 'months')
+        return self._months
         
     def computePmt(self):
         # formula: pmt = PV*(r*(1+r)**n)/((1+r)**months -1)
@@ -30,15 +33,6 @@ class loan(object):
         self._Pmt = self._PV*(r*(1+r)**self._months)/((1+r)**self._months-1)
         print('Monthly payment = $', round(self._Pmt,2))
         return self._Pmt
-
-    def makePayment(self, amount):
-        self.amount = amount
-        return self.amount
-
-    def getBalance(self):
-        self.total = self._Pmt * self._months - self.amount
-        print(f"Thank you for your payment of ${self.amount}")
-        print(f"New Balance = ${self.total:.2f}")
 
 # add a class to example class
 # class creditCards(loan):
@@ -48,16 +42,27 @@ class loan(object):
 # function to get monthly total payments to CCs    
 class creditCards(loan):
     def __init__(self, name):
-        super().__init__(name)        
+        super().__init__(name)    
+
+    def makePayment(self, amount):
+
+        self.amount = amount
+        print("Thank you for your payment of $", self.amount)
+        return self.amount
+
+    def getBalance(self):
+        self.total = self._Pmt * self._months - self.amount
+        print(f"New Balance = ${self.total:.2f}")
+
 
 if __name__ == "__main__":
-    loan1 = loan('Dr J')
-    loan1.who()
+    # loan1 = loan('Dr J')
+    # loan1.who()
 
-    loan1.setPV(27150)  #mini cooper
-    loan1.setRate(1.9)
-    loan1.setMonths(42)
-    payment = loan1.computePmt()
+    # loan1.setPV(27150)  #mini cooper
+    # loan1.setRate(1.9)
+    # loan1.setMonths(42)
+    # payment = loan1.computePmt()
 
     credit1 = creditCards("Amex")
     credit1.who()
@@ -68,13 +73,13 @@ if __name__ == "__main__":
 
     credit1.makePayment(500)
     credit1.getBalance()
+    
+    credit2 = creditCards("Amex")
+    credit2.who()
+    credit2.setPV(1000)
+    credit2.setRate(10.0)
+    credit2.setMonths(12)
+    payment = credit2.computePmt()
 
-    credit1 = creditCards("Discover")
-    credit1.who()
-    credit1.setPV(2000)
-    credit1.setRate(18)
-    credit1.setMonths(12)
-    payment = credit1.computePmt()
-
-    credit1.makePayment(500)
-    credit1.getBalance()
+    credit2.makePayment(500)
+    credit2.getBalance()
