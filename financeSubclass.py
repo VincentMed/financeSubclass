68eb9480a1d4e44aa53f2a985cf564c6c550aec4
Vincent Medrano <90278614+VincentMed@users.noreply.github.com>
2022-11-20 14:29:49 -0800

    Update financeSubclass.py

--- financeSubclass.py
+++ financeSubclass.py
@@ -2,17 +2,11 @@
 """
 Created on Mon Nov  7 09:07:39 2022
 @author: Vincent Medrano
-add a class to example class
-class creditCards(finance):
-sub class of finance, contains at least 2 credit cards : name, balance
-and monthly payment.
-function to get total owed
-function to get monthly total payments to CCs
 """
 
 class loan(object):
     def __init__(self, name):
-        self._name= name
+        self._name = name
         
     def who(self):
         print(self._name)
@@ -34,23 +28,27 @@ class loan(object):
         # formula: pmt = PV*(r*(1+r)**n)/((1+r)**months -1)
         r = self._ratePct/100/12
         self._Pmt = self._PV*(r*(1+r)**self._months)/((1+r)**self._months-1)
-        print('payment = $', round(self._Pmt,2))
+        print('Monthly payment = $', round(self._Pmt,2))
         return self._Pmt
-    
-class creditCards(object):
-    def __init__(self, cc_name, cc_balance, cc_APR):
-        self.cc_APR = cc_APR/100
-        self.cc_name = cc_name
-        self.cc_balance = cc_balance
-        self.monthly_interest = (self.cc_APR/12)*self.cc_balance
-        self.total_monthly = self.cc_balance/12+self.monthly_interest
-        
-        
-        print(f"\n{self.cc_name} balance: ${self.cc_balance}")
-        print(f"\nMonthly interest is: ${self.monthly_interest:.2f}")
-        print(f"\nTotal monthly payment: ${self.total_monthly:.2f}")
 
-        
+    def makePayment(self, amount):
+        self.amount = amount
+        return self.amount
+
+    def getBalance(self):
+        self.total = self._Pmt * self._months - self.amount
+        print(f"Thank you for your payment of ${self.amount}")
+        print(f"New Balance = ${self.total:.2f}")
+
+# add a class to example class
+# class creditCards(loan):
+# sub class of finance, contains at least 2 credit cards : name, balance
+# and monthly payment.
+# function to get total owed
+# function to get monthly total payments to CCs    
+class creditCards(loan):
+    def __init__(self, name):
+        super().__init__(name)        
 
 if __name__ == "__main__":
     loan1 = loan('Dr J')
@@ -60,6 +58,23 @@ if __name__ == "__main__":
     loan1.setRate(1.9)
     loan1.setMonths(42)
     payment = loan1.computePmt()
-    
-    credit1 = creditCards("Amex", 600, 12)
-    credit2 = creditCards("Discover", 500, 20.5)
+
+    credit1 = creditCards("Amex")
+    credit1.who()
+    credit1.setPV(1000)
+    credit1.setRate(10.0)
+    credit1.setMonths(12)
+    payment = credit1.computePmt()
+
+    credit1.makePayment(500)
+    credit1.getBalance()
+
+    credit1 = creditCards("Discover")
+    credit1.who()
+    credit1.setPV(2000)
+    credit1.setRate(18)
+    credit1.setMonths(12)
+    payment = credit1.computePmt()
+
+    credit1.makePayment(500)
+    credit1.getBalance()
